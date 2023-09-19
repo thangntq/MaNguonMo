942f71a29dcc0949f04faaffe8dca345a7aab8e9
HoangQuocTy <145129342+HoangQuocTy@users.noreply.github.com>
2023-09-19 19:07:46 +0700

    Update baighpt.py

--- baighpt.py
+++ baighpt.py
@@ -1,72 +1,64 @@
+# dùng hàm slove
+#tạo giao diện
+#nhập n tùy ý, có đk n>1
+#xét vô nghiệm, vô số nghiệm sd hàm det
 import numpy as np
 import tkinter as tk
-from tkinter import Entry, Label, Button, messagebox
+from tkinter import messagebox, simpledialog
+
+
+def get_matrix_input(n):
+    matrix = np.zeros((n, n))
+    for i in range(n):
+        row_input = simpledialog.askstring("Nhập ma trận A",
+                                           f"Nhập hàng {i + 1} (cách nhau bởi dấu cách hoặc dấu phẩy):")
+        row = [float(x) for x in row_input.split()]
+        matrix[i] = row
+    return matrix
+
+
+def get_vector_input(n):
+    vector_input = simpledialog.askstring("Nhập vector B", "Nhập vector B (cách nhau bởi dấu cách hoặc dấu phẩy):")
+    vector = [float(x) for x in vector_input.split()]
+    return vector
+
 
 def solve_equation():
+    n = 0
+    while n <= 1:
+        try:
+            n = int(simpledialog.askstring("Nhập số lượng biến", "Nhập số lượng biến (n > 1):"))
+            if n <= 1:
+                messagebox.showerror("Lỗi", "Số lượng biến phải lớn hơn 1. Vui lòng nhập lại.")
+        except ValueError:
+            messagebox.showerror("Lỗi", "Vui lòng nhập một số nguyên lớn hơn 1.")
+
+    A = get_matrix_input(n)
+    B = get_vector_input(n)
+
     try:
-        # Lấy số biến (n)
-        n = int(variables_entry.get())
+        det_A = np.linalg.det(A)
 
-        # Tạo ma trận A từ các giá trị nhập vào
-        A = np.zeros((n, n))
-        for i in range(n):
-            for j in range(n):
-                A[i][j] = float(coeff_entries[i][j].get())
+        if det_A == 0:
+            messagebox.showinfo("Kết quả", "Hệ phương trình có vô số nghiệm hoặc không có nghiệm.")
+        else:
+            X = np.linalg.solve(A, B)
+            result_str = "Nghiệm của hệ phương trình:\n"
+            for i in range(n):
+                result_str += f"x{i + 1} = {X[i]}\n"
+            messagebox.showinfo("Kết quả", result_str)
+    except np.linalg.LinAlgError:
+        messagebox.showerror("Lỗi", "Hệ phương trình không có nghiệm hoặc có nghiệm vô số.")
+    except ValueError:
+        messagebox.showerror("Lỗi", "Lỗi: Hệ phương trình không hợp lệ.")
 
-        # Tạo vector cột B từ các giá trị nhập vào
-        B = np.zeros((n, 1))
-        for i in range(n):
-            B[i] = float(constant_entries[i].get())
 
-        # Giải hệ phương trình
-        X = np.linalg.solve(A, B)
-
-        # Hiển thị nghiệm trên giao diện
-        solution_label.config(text="Nghiệm của hệ phương trình (X): " + ", ".join(map(str, X)))
-
-    except ValueError as e:
-        messagebox.showerror("Lỗi", "Giá trị nhập không hợp lệ. Vui lòng kiểm tra lại.")
-
-# Tạo cửa sổ Tkinter
+# Tạo cửa sổ giao diện
 root = tk.Tk()
-root.title("Ứng dụng giải hệ phương trình tuyến tính")
+root.title("Giải hệ phương trình tuyến tính")
 
-# Tạo nhãn và ô nhập dữ liệu cho số biến
-variables_label = Label(root, text="Số biến (n):")
-variables_label.pack()
-variables_entry = Entry(root)
-variables_entry.pack()
-
-coeff_entries = []
-constant_entries = []
-
-# Hàm tạo các ô nhập dữ liệu dựa trên số biến n sau khi nhấn nút "Tạo"
-def create_entries():
-    n = int(variables_entry.get())
-    for i in range(n):
-        row_entries = []
-        for j in range(n):
-            coeff_label = Label(root, text=f"Hệ số {i + 1}{j + 1}:")
-            coeff_label.pack()
-            coeff_entry = Entry(root)
-            coeff_entry.pack()
-            row_entries.append(coeff_entry)
-        coeff_entries.append(row_entries)
-
-    for i in range(n):
-        const_label = Label(root, text=f"Hằng số {i + 1}:")
-        const_label.pack()
-        const_entry = Entry(root)
-        const_entry.pack()
-        constant_entries.append(const_entry)
-
-    solve_button = Button(root, text="Giải", command=solve_equation)
-    solve_button.pack()
-
-solution_label = Label(root, text="")
-solution_label.pack()
-
-create_button = Button(root, text="Tạo", command=create_entries)
-create_button.pack()
+# Tạo nút để khởi động giải hệ phương trình
+solve_button = tk.Button(root, text="Giải hệ phương trình", command=solve_equation)
+solve_button.pack(pady=20)
 
 root.mainloop()
